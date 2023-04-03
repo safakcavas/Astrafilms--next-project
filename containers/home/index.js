f547ec59e98c5224535bd75af9d2ced7876ebf4e
safakcavas <safakcvs@gmail.com>
2023-04-04 00:07:40 +0300

    .

--- containers/home/index.js
+++ containers/home/index.js
@@ -11,7 +11,7 @@ function HomeContainer({
   
   return (
     <div>
-      <FeaturedMovie movie={topRatedMovies?.[0]} />
+      <FeaturedMovie movie={topRatedMovies?.[1]} />
       <Categories categories={categoriess.slice(0, 5)} /> 
       
       { selectedCategory.movies.length > 0 && (
